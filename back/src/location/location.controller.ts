4f1d46ffe172d8e0d781ef966132706ccf46a5a1
Cristalia <ericard@student.42.fr>
2022-09-09 18:15:53 +0200

    Feat: Search Location

--- back/src/location/location.controller.ts
+++ back/src/location/location.controller.ts
@@ -1,5 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get} from '@nestjs/common';
+import {LocationService} from "./location.service";
+import {LocationDto} from "../dto/location.dto";
 
 @Controller('location')
 export class LocationController {
+	constructor(private readonly locationService: LocationService) {}
+
+	@Get()
+	async searchLocation(@Body() location: LocationDto) {
+		return await this.locationService.verifyLocation(location.city, location.country)
+	}
+
 }
