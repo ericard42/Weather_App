e98f069b6d377d5849a0c3eb6b6495d189480ee8
ericard42 <ericard@student.42.fr>
2022-09-10 19:04:35 +0200

    Feat: Middleware for log all requests

---
+++ back/src/middleware/app-logger.middleware.ts
@@ -0,0 +1,22 @@
+import {Injectable, NestMiddleware, Logger, HttpStatus, ForbiddenException} from "@nestjs/common";
+import {Request, Response} from "express";
+
+@Injectable()
+export class AppLoggerMiddleware implements NestMiddleware {
+	private logger = new Logger('HTTP')
+	constructor() {}
+
+	use(req: Request, res: Response, next: () => void) {
+		const {headers, method, originalUrl: url} = req
+		//Autoriser uniquement le Front + Localhost + Postman
+		if (req.ip !== "::ffff:127.0.0.1" && req.ip !== "::1")
+			res.statusCode = HttpStatus.FORBIDDEN
+		res.on('close', () => {
+			const {statusCode} = res;
+			this.logger.log(`${method} ${url} ${statusCode} - ${headers["origin"]}`)
+		})
+		if (res.statusCode === HttpStatus.FORBIDDEN)
+			throw new ForbiddenException()
+		next();
+	}
+}
