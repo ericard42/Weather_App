4503b2ac47b836f684ebe4eedfc15b3a6d0d1c4e
Cristalia <ericard@student.42.fr>
2022-09-12 16:59:57 +0200

    Fix: Clean sign-in functions

--- front/pages/sign-in.js
+++ front/pages/sign-in.js
@@ -3,25 +3,26 @@ import Head from "next/head";
 import {MyHead} from "./index";
 import styles from '../styles/Sign-In.module.css'
 
+function FormBox({type, name}) {
+    return (
+        <div className={styles.field_box}>
+            <h4 className={styles.form_text}>{name} :</h4>
+            <input type={type} id={type} className={styles.form}/>
+        </div>
+    )
+}
+
 function LoginBox() {
     return (
         <div className={styles.login_box}>
             <div className={styles.fields}>
                 <h2 className={styles.login_text}>Login</h2>
-                <div className={styles.field_box}>
-                    <h4 className={styles.form_text}>Email :</h4>
-                    <input type={"email"} id={"password"} className={styles.form}/>
-                </div>
-                     <div className={styles.field_box}>
-                     <h4 className={styles.form_text}>Password :</h4>
-                <input type={"password"} id={"password"} className={styles.form}/>
-                </div>
+                <FormBox type={'email'} name={"Email"}/>
+                <FormBox type={'password'} name={"Password"}/>
                 <Link href={"/"}>
-                    <a>
-                        <p className={styles.login_button}>
-                            Login
-                         </p>
-                    </a>
+                    <a><p className={styles.login_button}>
+                        Login
+                    </p></a>
                 </Link>
             </div>
         </div>
